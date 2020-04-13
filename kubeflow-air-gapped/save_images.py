8328905ecca0df1d0b7a29017bda2e88881e7665
Chua Song Ann <chuasongann@gmail.com>
2020-04-13 10:23:28 +0800

    Update save_images.py

--- kubeflow-air-gapped/save_images.py
+++ kubeflow-air-gapped/save_images.py
@@ -95,11 +95,11 @@ original_image = [
     "tensorflow/tensorflow:1.8.0",
 ]
 
-# Uncomment when pulling and tagging of images(while connected to Internet)
+# # Uncomment when pulling and tagging of images(while connected to Internet)
 # for i in range(len(original_image)):
 #     os.system("docker pull %s"%original_image[i])
 #     os.system("docker tag %s %s"%(original_image[i],private_registry+"/"+original_image[i]))
 
-# Uncomment when pushing to private Docker registry
+# # Uncomment when pushing to private Docker registry
 # for i in range(len(original_image)):
 #     os.system("docker push %s"%private_registry+"/"+original_image[i])
